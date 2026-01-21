17049bc102055318d2f2ec83ed12630e725f07e1
Nico Hede <nico.hede@esedulainen.fi>
2026-01-21 12:47:39 +0200

    anekdoonit lisättiin komponentit

--- osa1/anekdootit/App.jsx
+++ osa1/anekdootit/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
 
   return (
     <div>
@@ -16,9 +19,18 @@ const App = () => {
 
       <h1>statistics</h1>
 
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
+      {all === 0 ? (
+        <div>No feedback given</div>
+      ) : (
+        <div>
+          <div>good {good}</div>
+          <div>neutral {neutral}</div>
+          <div>bad {bad}</div>
+          <div>all {all}</div>
+          <div>average {average}</div>
+          <div>positive {positive} %</div>
+        </div>
+      )}
     </div>
   )
 }
